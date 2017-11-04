f8a21f2ac7387b40de6cdbf37d9eb2d171a07834
Your Name <origamillamaa@gmail.com>
2017-11-04 18:15:54 +0000

    more accurate

--- ex02/rng.py
+++ ex02/rng.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if os.path.exists('data/numbers.csv'):
         with open('data/numbers.csv', 'w') as csvfile:
             writer = csv.writer(csvfile, delimiter = ',')
-            for i in range(300):
+            for i in range(1000):
                 arr = []
                 maxn = 0
                 for i in range(10):
